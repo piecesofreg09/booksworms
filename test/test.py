80c3dacc5f7447d3d59ca8c6737230ca9c0db179
Lu, Hongfang <hlu82@gatech.edu>
2018-04-24 15:44:28 -0400

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,19 @@
+from bookworms.train import train
+from bookworms.visualize import visualize
+
+
+def test():
+    print('training input json')
+    train(['input.json'], 10)
+    
+    print('')
+    print('when no input file is specified')
+    train([], 10)
+    
+    print('')
+    print('visualize')
+    visualize()
+
+
+if __name__ == '__main__':
+    test()
