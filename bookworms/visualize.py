aed61e6a47f255429f4f58defd723e0ad91880a0
Lu, Hongfang <hlu82@gatech.edu>
2018-04-26 08:49:15 -0400

    Update visualize.py

--- bookworms/visualize.py
+++ bookworms/visualize.py
@@ -16,11 +16,18 @@ import random
 import json
 
 def visualize():
+    # visualize() takes no input argument, as intput argument should be generated
+    # from the training process. If train is not called, visualize() will use the 
+    # predownloaded data for visualization
+    
     
     try:
+        # open input data
         with open('reader_basket', 'r') as input:
             reader_basket = json.load(input)
     except:
+        # if no input data is generated, sample data from the folder 'sample_data'
+        # will be used
         print('------------------------------------------------------')
         print('No input "reader_basket" file is found. Using sample_data instead.')
         script_dir = os.path.dirname(__file__)
