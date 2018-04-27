ec1db55819a2e49d07f08e8549c029283d84ef92
Chu, Kevin <chu.k@gatech.edu>
2018-04-27 18:49:56 -0400

    viz files

--- bookworms/visualize.py
+++ bookworms/visualize.py
@@ -14,18 +14,20 @@ python visualize
 import sys, os
 import random
 import json
+from subprocess import call
+import bookworms
 
-def visualize(input_file):
-    # visualize() takes one input argument, which is unstructured, as it is
-    # required by the collaborative filtering algorithm
-    
+
+def visualize(input_file, run_dir):
+    # visualize() takes one input argument, as intput argument should be generated
+    # from the training process. If train is not called, visualize() will use the 
+    # predownloaded data for visualization
     
     try:
         # open input raw data
-        if len(input_file) < 1:
-            raise Exception('No input file')
-        else:
-            input_str = input_file
+        with open(input_file[0]) as file:
+            pass
+        input_str = input_file
     except:
         print('------------------------------------------------------')
         print('No input file is specified. Using sample_data instead.')
@@ -33,14 +35,16 @@ def visualize(input_file):
         rel_path = 'sample_data'
         file_name = 'input_100.json'
         sample_data_path = os.path.join(script_dir, rel_path, file_name)
-        input_str = [sample_data_path]
-    
-    # If train is not called, hence 'reader_basket' is not generated, 
-    # visualize() will use the predownloaded data for visualization
+        input_str = [file_name]
+
     try:
         # open input data
         with open('reader_basket', 'r') as input:
             reader_basket = json.load(input)
+
+        data_path = run_dir
+        print(data_path)
+        
     except:
         # if no input data is generated, sample data from the folder 'sample_data'
         # will be used
@@ -53,19 +57,23 @@ def visualize(input_file):
         # open the file
         with open(sample_data_path, 'r') as input:
             reader_basket = json.load(input)
-        
 
+        data_path = os.path.join(script_dir, rel_path)
+        
     # get the users and generate one random user    
     users = list(reader_basket.keys())
     random_user = random.choice(users)
     
-    # get the first book of the random user for friend-weighted algorithm
-    all_book_keys = list(reader_basket[random_user].keys())
-    first_book = all_book_keys[0]
-
     # prepare for the flask website visulization
     print('Setting web page for visualization')
-    # NEEDS IMPLEMENTATION
+    assert sys.version_info >= (3,6)
+    py_ver = str(sys.executable)
+    path = os.path.dirname(bookworms.visualize.__file__)
+    print(path)
+    call([py_ver, path + '/app.py', data_path, input_str[0]])
     
-    
-    
+def main():
+    visualize(['aslfd'])
+
+if __name__ == '__main__':
+    main()
